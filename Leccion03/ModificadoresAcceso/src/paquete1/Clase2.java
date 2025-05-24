db913d07424a95c1c5542cd6f2502189db13c942
Nicolás Fernández <nicolaz_alvert@hotmail.com>
2025-05-24 19:58:52 -0300

    Subida Clase 3 forEach, Autoboxing, Unboxing, Modificadores de acceso

---
+++ Leccion03/ModificadoresAcceso/src/paquete1/Clase2.java
@@ -0,0 +1,19 @@
+package paquete1;
+
+
+class Clase2 {
+    String atributoDefault = "Valor del atributo default";
+    
+    //Clase2 (){
+        //.out.println("Constructor Default");
+    //}
+    
+     Clase2(){
+       System.out.println("Constructor Default");
+    }
+    
+    
+    void metodoDefault(){
+        System.out.println("Método Default");
+    }
+}
